3fea45d2d58a54ba053cd1f3d2a093e33554a1cc
Bart Shaughnessy <bshaughn48@hotmail.com>
2014-11-15 13:35:38 -0800

    Added Overridden Nav Controllers for testing

---
+++ RFNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  RFNavigationController.h
+//  RangeFinder
+//
+//  Created by Bart Shaughnessy on 11/15/14.
+//  Copyright (c) 2014 CPL Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RFNavigationController : UINavigationController
+
+@end
