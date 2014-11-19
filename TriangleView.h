774234f249f468aa181762dece60d25bba3bdc69
Bart Shaughnessy <bshaughn48@hotmail.com>
2014-11-19 00:29:23 -0800

    Height View Controller redesign is unfinished. Last commit for tonight, animation work begins in the morning

---
+++ TriangleView.h
@@ -0,0 +1,13 @@
+//
+//  TriangleView.h
+//  RangeFinder
+//
+//  Created by Bart Shaughnessy on 11/18/14.
+//  Copyright (c) 2014 CPL Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TriangleView : UIView
+
+@end
---
+++ TriangleView.m
@@ -0,0 +1,21 @@
+//
+//  TriangleView.m
+//  RangeFinder
+//
+//  Created by Bart Shaughnessy on 11/18/14.
+//  Copyright (c) 2014 CPL Consulting. All rights reserved.
+//
+
+#import "TriangleView.h"
+
+@implementation TriangleView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
