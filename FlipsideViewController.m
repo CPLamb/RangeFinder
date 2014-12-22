5997a945c4bed232838684e55d3e52d4800b49b7
CPLamb <CPLamb@Pacbell.net>
2014-12-22 14:31:34 -0800

    Minor tweeks & polishing

--- FlipsideViewController.m
+++ FlipsideViewController.m
@@ -37,7 +37,7 @@
     
 // Define pickerView items
     heightItems = [[NSArray alloc] initWithObjects:@"0", @"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"11", @"12", @"13", @"14", @"15", @"16", @"17", @"18", @"19", @"20", @"21", @"22", @"23", @"24", @"25", @"26", @"27", @"28", @"29", @"30", @"31", @"32", @"33", @"34", @"35", @"36", @"37", @"38", @"39",@"40", @"41", @"42", @"43", @"44", @"45", @"46", @"47", @"48", @"49", @"50", @"51", @"52", @"53", @"54", @"55", @"56", @"57", @"58", @"59", @"60", @"61", @"62", @"63", @"64", @"65", @"66", @"67", @"68", @"69", @"70", @"71", @"72", @"73", @"74", @"75", @"76", @"77", @"78", @"79", @"80", @"81", @"82", @"83", @"84", @"85", @"86", @"87", @"88", @"89",@"90", @"91", @"92", @"93", @"94", @"95", @"96", @"97", @"98", @"99", @"100", @"120", @"140", @"160", @"180", @"200", @"250", @"300", @"350", @"400", nil];
-    unitsPicker = [[NSArray alloc] initWithObjects:@"inch", @"foot", @"yard", @"meter", @"kilo", @"mile", nil];
+    unitsPicker = [[NSArray alloc] initWithObjects:@"inch", @"foot", @"yard", @"meter", @"kmeter", @"mile", nil];
     objectPickerItems = [[NSArray alloc] initWithObjects:@"Light switch", @"Car", @"Person", @"Door", @"Golf flag", @"Utility pole", @"Sailboat", @"Lighthouse", nil];
         
 // sets defaults for the Picker
@@ -82,6 +82,10 @@
     NSDictionary *newDistantObject = [[NSDictionary alloc] initWithObjects:newObject forKeys:keys];
     [self.heightObjects addObject:newDistantObject];
     [self.objectPicker reloadAllComponents];
+    
+    // And then saves the mutableArray heightObjects to a NSUserDefaults
+//    NSUserDefaults *savedData = [NSUserDefaults standardUserDefaults];
+//    [savedData ]
 }
 
 - (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
